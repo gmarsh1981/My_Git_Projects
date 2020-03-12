1fbd5bb0588df189715c562fd5ff547dfca9a11e
Greg Marsh <gmarsh1981@gmail.com>
2020-03-12 11:48:40 -0700

    create vpn

---
+++ ocesvpn.ps1
@@ -0,0 +1,8 @@
+﻿$ServerAddress = "	oak-creek-energy-wlan-wired-krmnkmczzp.dynamic-m.com"
+
+$ConnectionName = "OCES VPN"
+
+$PresharedKey = "O4kCr33KVPN-2018!"
+Get-VpnConnection | Remove-VpnConnection
+
+Add-VpnConnection -Name "$ConnectionName" -ServerAddress "$ServerAddress" -TunnelType L2tp -AllUserConnection -L2tpPsk "$PresharedKey" -AuthenticationMethod Pap -RememberCredential -Force -PassThru -verbose

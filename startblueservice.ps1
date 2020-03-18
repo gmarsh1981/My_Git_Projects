fe707530a619bf0e8c42b31dcbfb8e0c444f97a5
Greg Marsh <gmarsh1981@gmail.com>
2020-03-18 15:23:04 -0700

    Create startblueservice.ps1

---
+++ startblueservice.ps1
@@ -0,0 +1,23 @@
+﻿Configuration startServiceBlue {
+    param(
+        [Parameter(mandatory=$true)]
+        [string]$computerName
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+    Node $computerName{
+        
+        Service bluetooth {
+            
+            Name = 'BTAGService'
+            Ensure = 'Present'
+            StartupType = 'Automatic'
+            State = 'Running'       
+        }
+    }
+}
+
+startServiceBlue -computerName LAPTOP-VSBJF5A4
+Start-DscConfiguration -ComputerName LAPTOP-VSBJF5A4 -path startServiceBlue -Force -Wait -Verbose
+test-DscConfiguration -Path 'C:\Users\gmarsh\Documents\PowerShell\Scripts\startServiceBlue' -ComputerName LAPTOP-VSBJF5A4
